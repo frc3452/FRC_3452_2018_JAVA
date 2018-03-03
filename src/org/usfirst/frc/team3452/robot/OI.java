b2384149356acc2bf22b69b82cb5cc840965e57d
maccopacco <maxdmax101@gmail.com>
2018-03-03 17:00:47 -0500

    changes

--- src/org/usfirst/frc/team3452/robot/OI.java
+++ src/org/usfirst/frc/team3452/robot/OI.java
@@ -1,12 +1,5 @@
 package org.usfirst.frc.team3452.robot;
 
-import org.usfirst.frc.team3452.robot.commands.drive.SetModify;
-import org.usfirst.frc.team3452.robot.commands.elevator.ElevatorManual;
-import org.usfirst.frc.team3452.robot.commands.elevator.ElevatorPosition;
-import org.usfirst.frc.team3452.robot.commands.elevator.OverrideSet;
-import org.usfirst.frc.team3452.robot.commands.pwm.Climb;
-import org.usfirst.frc.team3452.robot.commands.pwm.IntakeControl;
-import org.usfirst.frc.team3452.robot.commands.pwm.IntakeSpin;
 import org.usfirst.frc.team3452.robot.triggers.DriveSafteyOverriden;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -16,6 +9,7 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 public class OI {
 	public static Joystick driverJoy = new Joystick(0);
 	public static Joystick opJoy = new Joystick(1);
+
 	public static DriveSafteyOverriden driveSafteyOverriden = new DriveSafteyOverriden();
 
 	@SuppressWarnings("unused")
@@ -34,27 +28,27 @@ public class OI {
 
 	public static void init() {
 		// DRIVER JOY
-		driverJoyA.whenPressed(new SetModify(-1));
-
-		driverJoyBack.whenPressed(new OverrideSet(-1));
-
-		driverJoyX.whileHeld(new IntakeControl(-.75, .01));
-		driverJoyB.whileHeld(new IntakeControl(.75, 0.01));
-		driverJoyRB.whileHeld(new ElevatorManual(driverJoy));
-
-		// OP JOY
-		opJoyLB.whileHeld(new ElevatorManual(opJoy));
-		opJoyRB.whileHeld(new Climb(.7));
-
-		opJoyBack.whileHeld(new IntakeSpin(.35, true));
-		opJoyStart.whileHeld(new IntakeSpin(.35, false));
-
-		opJoyX.whileHeld(new IntakeControl(-.75, 0.01));
-		opJoyB.whileHeld(new IntakeControl(.75, 0.01));
-		opJoyY.whileHeld(new IntakeControl(.3, 0.01));
-		opJoyA.whenPressed(new ElevatorPosition(3));
-
-		driveSafteyOverriden.whenActive(new OverrideSet(1));
-		driveSafteyOverriden.whenInactive(new OverrideSet(0));
+		/*
+		 * driverJoyA.whenPressed(new SetModify(-1));
+		 * 
+		 * driverJoyBack.whenPressed(new OverrideSet(-1));
+		 * 
+		 * driverJoyX.whileHeld(new IntakeControl(-.75, .01));
+		 * driverJoyB.whileHeld(new IntakeControl(.75, 0.01));
+		 * driverJoyRB.whileHeld(new ElevatorManual(driverJoy));
+		 * 
+		 * // OP JOY opJoyLB.whileHeld(new ElevatorManual(opJoy));
+		 * opJoyRB.whileHeld(new Climb(.7));
+		 * 
+		 * opJoyBack.whileHeld(new IntakeSpin(.35, true));
+		 * opJoyStart.whileHeld(new IntakeSpin(.35, false));
+		 * 
+		 * opJoyX.whileHeld(new IntakeControl(-.75, 0.01)); opJoyB.whileHeld(new
+		 * IntakeControl(.75, 0.01)); opJoyY.whileHeld(new IntakeControl(.3,
+		 * 0.01)); opJoyA.whenPressed(new ElevatorPosition(3));
+		 * 
+		 * driveSafteyOverriden.whenActive(new OverrideSet(1));
+		 * driveSafteyOverriden.whenInactive(new OverrideSet(0));
+		 */
 	}
 }
