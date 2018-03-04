d0d91547a57b2c72b36834a4aa033df0dd002252
maccopacco <maxdmax101@gmail.com>
2018-03-04 18:09:40 -0500

    ugly comments fixed

--- src/org/usfirst/frc/team3452/robot/commands/auton/MiddleAuton.java
+++ src/org/usfirst/frc/team3452/robot/commands/auton/MiddleAuton.java
@@ -14,8 +14,7 @@ public class MiddleAuton extends CommandGroup {
 
 	public MiddleAuton(String priority, int selector) {
 		addSequential(new WaitForGameData());
-		
-		
+
 		if (priority == "SWITCH") {
 			if (Robot.lights.gsm().charAt(0) == 'L') {
 				switchL(selector);
@@ -41,57 +40,37 @@ public class MiddleAuton extends CommandGroup {
 		if (mode == 1) {
 			addParallel(new DriveTime(.25, 0, .5));
 			addSequential(new ElevatorTime(.5, .15));
-			addSequential(new DriveTime(-.25, 0, .225)); // jog forward
-															// backwards to drop
-															// arm
+			addSequential(new DriveTime(-.25, 0, .225)); // jog forward backwards to drop arm
 
-			addSequential(new EncoderFrom(1.45, 1.45, .3, .3, .4)); // INIT
-																	// DRIVE
+			addSequential(new EncoderFrom(1.45, 1.45, .3, .3, .4)); // INIT DRIVE
 
 			addParallel(new ElevatorPosition(4.2));
 			addSequential(new EncoderFrom(-0.38, .75, .5, .5, .5)); // turn left
 
-			addSequential(new EncoderFrom(4.9, 3.2, .4, .4, .5)); // drive to
-																	// scale
-			addSequential(new EncoderFrom(.4, .4, .4, .4, .4)); // .5 .5 .4 .4
-																// .5
+			addSequential(new EncoderFrom(4.9, 3.2, .4, .4, .5)); // drive to scale
+			addSequential(new EncoderFrom(.4, .4, .4, .4, .4)); // .5 .5 .4 .4 .5
 
 			addSequential(new IntakeTime(1, .5));
-			addSequential(new EncoderFrom(-1.4, -1.4, .3, .3, .2)); // back up
-																	// after
-																	// placing
+			addSequential(new EncoderFrom(-1.4, -1.4, .3, .3, .2)); // back up after placing
 
-			addSequential(new EncoderFrom(-.43 - .1, .75 + .1, .6, .6, .6)); // spin,
-																				// drive
-																				// around
-																				// switch
-																				// and
-																				// go
-																				// forward
+			addSequential(new EncoderFrom(-.43 - .1, .75 + .1, .6, .6, .6)); // spin, drive, around switch and go forward
 			addSequential(new EncoderFrom(4.85 - 1.1, 3.35 - .5, .3, .3, .5));
 			addSequential(new EncoderFrom(1.5, 1.5, .3, .3, .4));
 		} else if (mode == 2) {
 			addParallel(new DriveTime(.25, 0, .5));
 			addSequential(new ElevatorTime(.5, .15));
-			addSequential(new DriveTime(-.25, 0, .225)); // jog forward
-															// backwards to drop
-															// arm
+			addSequential(new DriveTime(-.25, 0, .225)); // jog forward backwards to drop arm
 
-			addSequential(new EncoderFrom(1.45, 1.45, .3, .3, .4)); // INIT
-																	// DRIVE
+			addSequential(new EncoderFrom(1.45, 1.45, .3, .3, .4)); // INIT DRIVE
 
 			addParallel(new ElevatorPosition(4.2));
 			addSequential(new EncoderFrom(-0.38, .75, .5, .5, .5)); // turn left
 
-			addSequential(new EncoderFrom(4.9, 3.2, .4, .4, .5)); // drive to
-																	// scale
-			addSequential(new EncoderFrom(.4, .4, .4, .4, .4)); // .5 .5 .4 .4
-																// .5
+			addSequential(new EncoderFrom(4.9, 3.2, .4, .4, .5)); // drive to scale
+			addSequential(new EncoderFrom(.4, .4, .4, .4, .4)); // .5 .5 .4 .4 .5
 
-			addSequential(new IntakeTime(1, .5));
-			addSequential(new EncoderFrom(-1.4, -1.4, .3, .3, .2)); // back up
-																	// after
-																	// placing
+			addSequential(new IntakeTime(1, 1));
+			addSequential(new EncoderFrom(-1.4, -1.4, .3, .3, .2)); // back up  after placing
 		} else {
 			defaultAuton();
 		}
