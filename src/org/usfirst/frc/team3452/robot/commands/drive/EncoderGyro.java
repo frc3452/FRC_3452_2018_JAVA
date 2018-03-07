b71b7f19f0a0e8a974422e975ddf81c538a7fddf
maccopacco <maxdmax101@gmail.com>
2018-03-07 05:23:09 -0500

    closed loop w/ gyro assist fixed

--- src/org/usfirst/frc/team3452/robot/commands/drive/EncoderGyro.java
+++ src/org/usfirst/frc/team3452/robot/commands/drive/EncoderGyro.java
@@ -2,15 +2,14 @@ package org.usfirst.frc.team3452.robot.commands.drive;
 
 import org.usfirst.frc.team3452.robot.Robot;
 
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-
 import edu.wpi.first.wpilibj.command.Command;
 
 public class EncoderGyro extends Command {
 
-	private double l_pos, r_pos, l_accel, r_accel, m_speed, c_gyro, i_gyro;
+	private double l_pos, r_pos, l_accel, r_accel, m_speed, c_gyro, t_gyro, k;
 
-	public EncoderGyro(double leftpos, double rightpos, double leftaccel, double rightaccel, double speed) {
+	public EncoderGyro(double leftpos, double rightpos, double leftaccel, double rightaccel, double speed,
+			double angle, double constant) {
 		requires(Robot.drive);
 
 		l_pos = leftpos;
@@ -18,32 +17,42 @@ public class EncoderGyro extends Command {
 		l_accel = leftaccel;
 		r_accel = rightaccel;
 		m_speed = speed;
-		i_gyro = 0;
+		t_gyro = angle;
 		c_gyro = 0;
+		k = constant;
 	}
 
 	protected void initialize() {
-		setTimeout(15);
-		i_gyro = Robot.drive.Gyro.getAngle();
+		setTimeout(10);
+		t_gyro = Robot.drive.Gyro.getAngle();
 	}
 
 	protected void execute() {
 		c_gyro = Robot.drive.Gyro.getAngle();
 
-		if (c_gyro > i_gyro) 
-			Robot.drive.MotionMagic(l_pos, r_pos, l_accel, r_accel, m_speed * (1 - (.03 * Math.abs(c_gyro))), m_speed);
-		else
-			Robot.drive.MotionMagic(l_pos, r_pos, l_accel, r_accel, m_speed, m_speed * (1 - (.03 * Math.abs(c_gyro))));
+		if (c_gyro < t_gyro + .4 && c_gyro > t_gyro - .4)
+			Robot.drive.MotionMagic(l_pos, r_pos, l_accel, r_accel, m_speed, m_speed);
 
+		else if (c_gyro > t_gyro)
+			Robot.drive.MotionMagic(l_pos, r_pos, l_accel, r_accel, m_speed * (1 - (k * Math.abs(c_gyro - t_gyro))),
+					m_speed);
+
+		else if (c_gyro < t_gyro)
+			Robot.drive.MotionMagic(l_pos, r_pos, l_accel, r_accel, m_speed,
+					m_speed * (1 - (k * Math.abs(c_gyro - t_gyro))));
+
+		System.out.println((1 - (k * Math.abs(c_gyro - t_gyro))));
 	}
 
 	protected boolean isFinished() {
-		return Robot.drive.encoderIsDone(1.3) || isTimedOut();
+		return Robot.drive.encoderIsDoneEither(1.3) || isTimedOut();
 	}
 
 	protected void end() {
+		Robot.drive.EncoderDone();
 	}
 
 	protected void interrupted() {
+		end();
 	}
 }
