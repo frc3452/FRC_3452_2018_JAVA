5c5dce23db8d790832c8f29c30ec2a497e429ab5
maccopacco <maxdmax101@gmail.com>
2018-03-07 05:24:48 -0500

    imports?

--- src/org/usfirst/frc/team3452/robot/commands/pwm/Climb.java
+++ src/org/usfirst/frc/team3452/robot/commands/pwm/Climb.java
@@ -1,30 +1,34 @@
 package org.usfirst.frc.team3452.robot.commands.pwm;
 
 import org.usfirst.frc.team3452.robot.Robot;
-import org.usfirst.frc.team3452.robot.subsystems.Climber;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class Climb extends Command {
 	private double m_speed;
 
-    public Climb(double speed) {
-    	requires(Robot.climber);
-    	
-    	m_speed = speed;
-    }
-    protected void initialize() {
-    }
-    protected void execute() {
-    	Robot.climber.Control(m_speed);
-    }
-    protected boolean isFinished() {
-        return false;
-    }
-    protected void end() {
-    	Robot.climber.Control(0);
-    }
-    protected void interrupted() {
-    	end();
-    }
+	public Climb(double speed) {
+		requires(Robot.climber);
+
+		m_speed = speed;
+	}
+
+	protected void initialize() {
+	}
+
+	protected void execute() {
+		Robot.climber.Control(m_speed);
+	}
+
+	protected boolean isFinished() {
+		return false;
+	}
+
+	protected void end() {
+		Robot.climber.Control(0);
+	}
+
+	protected void interrupted() {
+		end();
+	}
 }
