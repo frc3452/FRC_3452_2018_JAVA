7365e02e1f19ac07a123b460ffffd8523ca2ae1d
maccopacco <maxdmax101@gmail.com>
2018-03-07 23:33:25 -0500

    pid changes

--- src/org/usfirst/frc/team3452/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team3452/robot/subsystems/Elevator.java
@@ -40,9 +40,9 @@ public class Elevator extends Subsystem {
 		// PIDs
 		Elev_1.config_kF(0, 0, 10);
 		Elev_1.config_kP(0, 0.09, 10);
-		Elev_1.config_kI(0, 0.000025, 10);
+		Elev_1.config_kI(0, 0.000045, 10);
 		Elev_1.config_kD(0, 2.5, 10);
-		// was .11, .00001, 1.11
+		// was 0, .09, .000025, 2.5
 
 		// RESET ENCODER ON LIMIT SWITCH DOWN
 		Elev_1.configSetParameter(ParamEnum.eClearPosOnLimitF, 1, 0, 0, 10);
