6091f7ffb0cde81c188f0b361f9ba5d77c3245cc
maccopacco <maxdmax101@gmail.com>
2018-03-14 08:46:00 -0400

    Update OI.java

--- src/org/usfirst/frc/team3452/robot/OI.java
+++ src/org/usfirst/frc/team3452/robot/OI.java
@@ -61,6 +61,25 @@ public class OI {
 		driveSafteyOverriden.whenActive(new OverrideSet(1));
 		driveSafteyOverriden.whenInactive(new OverrideSet(0));
 	}
+	
+	public void rumble(int controller, double intensity)
+	{
+	if (controller == 1)
+		{
+		driverJoy.setRumble(RumbleType.kLeftRumble, intensity);
+		driverJoy.setRumble(RumbleType.kRightRumble, intensity);
+		} else if (controller == 2)
+	{
+		opJoy.setRumble(RumbleType.kLeftRumble, intensity);
+		opJoy.setRumble(RumbleType.kRightRumble, intensity);} else if (controller == 3)
+	{
+		driverJoy.setRumble(RumbleType.kLeftRumble, intensity);
+		driverJoy.setRumble(RumbleType.kRightRumble, intensity);
+			opJoy.setRumble(RumbleType.kLeftRumble, intensity);
+		opJoy.setRumble(RumbleType.kRightRumble, intensity);
+	}
+	
+	}
 
 	private static void buttonInit() {
 		driverJoy = new Joystick(0);
