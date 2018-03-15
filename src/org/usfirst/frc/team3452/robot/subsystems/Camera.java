7b6a3f9cdfcd1f2115b97509f7ac43d6c6cb9799
maccopacco <maxdmax101@gmail.com>
2018-03-15 01:47:43 -0400

    exposure changes

--- src/org/usfirst/frc/team3452/robot/subsystems/Camera.java
+++ src/org/usfirst/frc/team3452/robot/subsystems/Camera.java
@@ -10,9 +10,9 @@ public class Camera extends Subsystem {
 
 	public void initHardware() {
 		mCamera0 = CameraServer.getInstance().startAutomaticCapture(0);
-		mCamera0.setResolution(320, 240);
+		mCamera0.setResolution(640, 480);
 		mCamera0.setFPS(30);
-		mCamera0.setExposureAuto();
+		mCamera0.setExposureManual(50);
 
 //		mCamera1 = CameraServer.getInstance().startAutomaticCapture(1);
 //		mCamera1.setResolution(180, 120);
