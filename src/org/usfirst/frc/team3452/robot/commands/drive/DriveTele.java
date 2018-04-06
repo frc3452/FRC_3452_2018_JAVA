6c3f057ee8991a280d7ffa5587a354d711f0b028
3452 <3452@GZ3452>
2018-04-05 23:25:14 -0400

    rumble added for override

--- src/org/usfirst/frc/team3452/robot/commands/drive/DriveTele.java
+++ src/org/usfirst/frc/team3452/robot/commands/drive/DriveTele.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3452.robot.commands.drive;
 
 import org.usfirst.frc.team3452.robot.OI;
 import org.usfirst.frc.team3452.robot.Robot;
+import org.usfirst.frc.team3452.robot.subsystems.Drivetrain.CONTROLLER;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -17,6 +18,10 @@ public class DriveTele extends Command {
 	protected void execute() {
 		Robot.drive.Arcade(OI.driverJoy);
 //		Robot.drive.Tank(OI.driverJoy);
+		if (Robot.elevator.m_overriden)
+			OI.rumble(CONTROLLER.DRIVER, .45);
+		else
+			OI.rumble(CONTROLLER.DRIVER, 0);
 	}
 
 	protected boolean isFinished() {
@@ -24,6 +29,7 @@ public class DriveTele extends Command {
 	}
 
 	protected void end() {
+		OI.rumble(CONTROLLER.DRIVER, 0);
 		Robot.drive.Arcade(0,0);
 	}
 
