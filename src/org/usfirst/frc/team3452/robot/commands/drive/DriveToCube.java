d039b88b6c7983ac502a60aa4baabf581a99d730
3452 <3452@10.0.2.206>
2018-04-13 01:20:12 -0400

    updated

--- src/org/usfirst/frc/team3452/robot/commands/drive/DriveToCube.java
+++ src/org/usfirst/frc/team3452/robot/commands/drive/DriveToCube.java
@@ -1,64 +1,86 @@
 package org.usfirst.frc.team3452.robot.commands.drive;
 
 import org.usfirst.frc.team3452.robot.Robot;
+import org.usfirst.frc.team3452.robot.subsystems.Intake;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveToCube extends Command {
 
-	private double m_speed, m_centerX;
-	private boolean m_complete, flag_1, flag_2;
+	private double m_speed, m_rotation, i_lpos, i_rpos, m_centerX;
+	private boolean m_complete, flag_1, flag_2, timeoutSet;
+	private Timer timer = new Timer();
 
-	public DriveToCube(double speed) {
+	public DriveToCube(double speed, double rotationLimit) {
 		requires(Robot.drive);
 
 		m_speed = speed;
+		m_rotation = rotationLimit;
 	}
 
 	protected void initialize() {
-		setTimeout(8);
+		setTimeout(10);
+
 		m_complete = false;
 		flag_1 = false;
 		flag_2 = false;
+		timeoutSet = false;
+
+		i_lpos = (double) Robot.drive.L1.getSelectedSensorPosition(0) / 4096;
+		i_rpos = (double) -Robot.drive.R1.getSelectedSensorPosition(0) / 4096;
+		
+		System.out.println(i_lpos);
+		System.out.println(i_rpos);
+
+		timer.stop();
+		timer.reset();
+		timer.start();
 	}
 
 	protected void execute() {
-		if (Robot.lights.centerX(0) == 3452)
-			m_centerX = 320;
-		else
-			m_centerX = Robot.lights.centerX(0);
+		//		if (Robot.lights.centerX(0) == 3452)
+		//			m_centerX = 320;
+		//		else
+		//			m_centerX = Robot.lights.centerX(0);
+		//
+		//		if (Robot.lights.visionLength() > 0) {
+		//
+		//			if (m_centerX < 335 && m_centerX > 325) {
+		//				Robot.drive.Arcade(m_speed, 0);
+		//
+		//			} else if (m_centerX > 335) {
+		//
+		//				Robot.drive.Arcade(m_speed, .125 * 2.1);
+		//
+		//			} else if (m_centerX < 325) {
+		//
+		//				Robot.drive.Arcade(m_speed, -.125 * 2.1);
+		//
+		//			}
+		//		} else {
+		//		}
+		Robot.drive.Arcade(m_speed, 0);
 
-		if (Robot.lights.visionLength() > 0) {
+		Robot.intake.manual(Intake.Speeds.INTAKE);
 
-			if (m_centerX < 335 && m_centerX > 325) {
-				Robot.drive.Arcade(m_speed, 0);
-
-			} else if (m_centerX > 335) {
-
-				Robot.drive.Arcade(m_speed, .125 * 2.1);
-
-			} else if (m_centerX < 325) {
-
-				Robot.drive.Arcade(m_speed, -.125 * 2.1);
-
-			}
-		} else {
-			Robot.drive.Arcade(m_speed, 0);
-		}
-
-		Robot.intake.manual(-.9);
-
-		if (Robot.drive.pdp.getCurrent(9) > 8 || Robot.drive.pdp.getCurrent(8) > 8)
+		if (((double) Robot.drive.L1.getSelectedSensorPosition(0) / 4096) - i_lpos > m_rotation
+				|| ((double) Robot.drive.R1.getSelectedSensorPosition(0) / 4096) - i_rpos > m_rotation)
+			m_complete = true;
+		
+		if (Robot.drive.pdp.getCurrent(9) > 12 || Robot.drive.pdp.getCurrent(8) > 12)
 			flag_1 = true;
 
-		if (flag_1 && (Robot.drive.pdp.getCurrent(9) < 6 || Robot.drive.pdp.getCurrent(8) < 6))
+		if (flag_1 && (Robot.drive.pdp.getCurrent(9) < 7 || Robot.drive.pdp.getCurrent(8) < 7))
 			flag_2 = true;
 
-		if (flag_2 && (Robot.drive.pdp.getCurrent(9) > 7.5 || Robot.drive.pdp.getCurrent(8) > 7.5))
-			setTimeout(1.2);
+		if (flag_2 && (Robot.drive.pdp.getCurrent(9) > 12 || Robot.drive.pdp.getCurrent(8) > 12)) {
 
-		System.out.println(m_centerX);
-		//		System.out.println(Robot.drive.pdp.getCurrent(9) + "\t\t" + Robot.drive.pdp.getCurrent(8));
+			if (!timeoutSet) {
+				setTimeout(timer.get() + .4);
+				timeoutSet = true;
+			}
+		}
 	}
 
 	protected boolean isFinished() {
