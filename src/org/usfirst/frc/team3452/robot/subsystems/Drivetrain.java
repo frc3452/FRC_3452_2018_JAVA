e070a8217c812f52ef7b685960c3e94ffdf694af
mac <mac@192.168.1.18>
2018-08-20 02:24:59 -0400

    Imports fixed
    >Drivetrain.java

--- src/org/usfirst/frc/team3452/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team3452/robot/subsystems/Drivetrain.java
@@ -12,7 +12,6 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
-import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
 
