a3e38f73e4f0f486ab9aa71f20a4c7bbc97268a5
maccopacco <maxdmax101@gmail.com>
2018-12-17 08:59:57 -0500

    comments added for abstract class

--- src/main/java/frc/robot/ExampleAbstractClass.java
+++ src/main/java/frc/robot/ExampleAbstractClass.java
@@ -1,23 +1,25 @@
 package frc.robot;
 
 public abstract class ExampleAbstractClass {
+    //Private variable
     private double mNumberWeWantToDoSomethingTo;
 
+    //Constructor
     public ExampleAbstractClass(double startingNumber)
     {
         setNumberTo(startingNumber);
     }
 
+    //Abstract method, when we create this variable we will have to define it there
     public abstract void doSomethingWithNumber();
 
+    //Set number to
     public void setNumberTo(double newNumber)
     {
         this.mNumberWeWantToDoSomethingTo = newNumber;
     }
 
-    public double getNumber()
-    {
-        return mNumberWeWantToDoSomethingTo;
-    }
+    //Get number
+    public double getNumber() { return this.mNumberWeWantToDoSomethingTo; }
 
 }
